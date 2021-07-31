c3da994be2dbcee2553a2f42ac510a04ce4b5fd2
Ramzi <ramzialhaddadtm@gmail.com>
2021-07-31 03:50:57 -0500

    reworked and solved

--- Problem19/CountingSundays/Program.cs
+++ Problem19/CountingSundays/Program.cs
@@ -19,18 +19,16 @@ namespace CountingSundays
 {
 	class Program
 	{
-		enum Days
+		public static void Main(string[] args)
 		{
-			Monday,
-			Tuesday,
-			Wednesday,
-			Thursday,
-			Friday,
-			Saturday,
-			Sunday
-		}
-		static void Main(string[] args)
-		{
+			int sundays = 0;
+
+			for (int year = 1901; year <= 2000; year++)
+				for (int month = 1; month <= 12; month++)
+					if ((new DateTime(year, month, 1)).DayOfWeek == DayOfWeek.Sunday)
+						sundays++;
+
+			Console.WriteLine($"Total Sunday Days = {sundays}");
 		}
 	}
 }
